a77b1410206895011066142fdef1f116c74423bb
emil14 <emil.musician@gmail.com>
2018-03-14 17:37:59 +0300

    Refactor simple search and Init npm module

--- index.js
+++ index.js
@@ -2,7 +2,10 @@ const { sorted } = require('./data');
 const { r, l } = require('./simple_search');
 
 const algorithms = {
-  simpleSearch: () => `r: ${r(sorted, 0, 24)} l: ${l(sorted, 24)}`,
+  simpleSearch: {
+    r: r(sorted, 0, 24),
+    l: l(sorted, 24),
+  },
 };
 
-Object.keys(algorithms).forEach(key => console.log(algorithms[key]()));
+for (let key in algorithms) { console.log(algorithms[key]) }
