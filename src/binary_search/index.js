8823b2f1470b2854f71c24767807ec1746f93270
Emil Valeev <emil.musician@gmail.com>
2018-05-29 13:33:09 +0300

    Rewrite Binary Search

--- src/binary_search/index.js
+++ src/binary_search/index.js
@@ -1,6 +1,30 @@
-function bn(arr, trgt) {
-  const i = Math.round(arr.length / 2), el = arr[i];
-  return el === trgt ? i : bn(el < trgt ? arr.slice(i) : arr.slice(0, i), trgt); // FIXME need absolute index
+function binarySearch(input, target) {
+  // Base case:
+  const inputLength = input.length
+  
+  if (inputLength === 0) {
+    return null
+  }
+  if (inputLength === 1) {
+    return 0
+  }
+  
+  const averageIndex = Math.round(inputLength / 2)
+  const averageElement = input[averageIndex]
+  
+  if (averageElement === trgt) {
+    return averageIndex;
+  }
+  
+  // Recursive case:
+  if (averageElement > target) {
+    const newInput = input.slice(0, averageElement)
+    binarySearch(newInput, target)
+  }
+  if (averageElement < target) {
+    const newInput = input.slice(averageElement)
+    binarySearch(newInput, target)
+  }
 }
 
-exports.bn = bn;
+exports.binarySearch = binarySearch
