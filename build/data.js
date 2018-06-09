ffabedad430c0f72a5e9f98da2360b98be3c08b6
emil14 <emil.musician@gmail.com>
2018-06-09 21:34:49 +0300

    Remplace ts-node with plain nodemon+tsc chain due to bugs

--- build/data.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const sortedNumList = [];
-exports.sortedNumList = sortedNumList;
-for (let i = 0; i < 100; i++) {
-    sortedNumList.push(i);
-}
-//# sourceMappingURL=data.js.map
