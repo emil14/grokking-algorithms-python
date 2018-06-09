a4fc0599908103c530f32f966e5faac2c29a2d4e
emil14 <emil.musician@gmail.com>
2018-06-10 01:46:10 +0300

    Refactor and Add return statement to binarySearch

--- src/binarySearch.ts
+++ src/binarySearch.ts
@@ -1,28 +1,18 @@
-const binarySearch = (input: number[], targetValue: number): number | null | undefined => {
+const binarySearch = (input: number[], targetValue: number): number | null => {
   const inputLength = input.length
 
-  if (inputLength === 0) {
-    return null
-  } else if (inputLength === 1) {
-    const singleElementValue = input[0]
-    return singleElementValue === targetValue ? 0 : null
-  }
+  if (inputLength === 0) return null
+  else if (inputLength === 1) return input[0] === targetValue ? 0 : null
 
   const middleElementIndex = Math.round(inputLength)
   const middleElementValue = input[middleElementIndex]
 
-  if (middleElementValue === targetValue) {
-    return middleElementIndex
-  } else {
-    let newInput: number[] = []
-
-    if (middleElementValue > targetValue) {
-      newInput = input.slice(0, middleElementIndex)
-    } else if (middleElementValue < targetValue) {
-      newInput = input.slice(middleElementIndex)
-    }
-
-    binarySearch(newInput, targetValue)
+  if (middleElementValue === targetValue) return middleElementIndex
+  else {
+    const newInput: number[] = middleElementValue > targetValue
+      ? input.slice(0, middleElementIndex)
+      : input.slice(middleElementIndex)
+    return binarySearch(newInput, targetValue)
   }
 }
 
