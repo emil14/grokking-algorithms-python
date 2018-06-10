a58e67b183c0106e60b3e74c3df866119a655c74
emil14 <emil.musician@gmail.com>
2018-06-10 12:18:29 +0300

    Fix binary search!

--- src/binarySearch.ts
+++ src/binarySearch.ts
@@ -1,18 +1,18 @@
-const binarySearch = (input: number[], targetValue: number): number | null => {
+const binarySearch = (
+  input: number[], targetValue: number, low: number = 0, hight: number = input.length
+): number | null => {
   const inputLength = input.length
-
   if (inputLength === 0) return null
   else if (inputLength === 1) return input[0] === targetValue ? 0 : null
 
-  const middleItemIndex = Math.round(inputLength / 2)
+  const middleItemIndex = Math.round((low + hight) / 2)
   const middleItemValue = input[middleItemIndex]
 
   if (middleItemValue === targetValue) return middleItemIndex
   else {
-    const newInput: number[] = middleItemValue > targetValue
-      ? input.slice(0, middleItemIndex)
-      : input.slice(middleItemIndex)
-    return binarySearch(newInput, targetValue)
+    const newLow = targetValue > middleItemValue ? middleItemIndex : 0
+    const newHight = targetValue > middleItemValue ? hight : middleItemIndex
+    return binarySearch(input, targetValue, newLow, newHight)
   }
 }
 
