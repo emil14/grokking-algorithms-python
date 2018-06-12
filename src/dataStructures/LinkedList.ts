eb95987eb68f56c9c47a4b7a6bd29f23b1459450
emil14 <emil.musician@gmail.com>
2018-06-12 21:25:05 +0300

    Implement LinkedList (WIP)

--- src/dataStructures/LinkedList.ts
+++ src/dataStructures/LinkedList.ts
@@ -1,7 +1,18 @@
+type Node = { data: any, next: Node | null }
+type Link = Node | null
+type LinkedList = { head: Link, tail: Link }
+
+const Node = (data: any, next = null): Node => ({ data, next })
+
 const LinkedList = () => {
+  const data: Node[] = []
+  let head: any = null
+  let tail = null
+
   return {
-    head: null,
-    tail: null,
+    addToHead: (value: any) => {
+      const node = Node(value, head)
+    }
   }
 }
 
