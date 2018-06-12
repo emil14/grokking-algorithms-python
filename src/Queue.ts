39686aab3d4e54347f208a201311964949498c9f
emil14 <emil.musician@gmail.com>
2018-06-12 19:31:54 +0300

    Implement Queue with closure

---
+++ src/Queue.ts
@@ -0,0 +1,21 @@
+type Queue = {
+  enqueue: (item: any) => number,
+  dequeue: () => any,
+  peek: () => any,
+  getLength: () => number,
+  isEmpty: () => boolean
+}
+
+const Queue = (): Queue => {
+  const data: Array<any> = []
+
+  return {
+    enqueue: item => data.push(item),
+    dequeue: () => data.pop(),
+    peek: () => data[data.length - 1] || null,
+    getLength: () => data.length,
+    isEmpty: () => data.length === 0
+  }
+}
+
+export default Queue
