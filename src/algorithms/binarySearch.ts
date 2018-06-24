c0e0b8afbc9aaf37e0cff7de4ef1d1e91d7070f5
Emil Valeev <emil.musician@gmail.com>
2018-06-24 16:27:06 +0300

    Refactor binrySearch:
    Get rid of unnecessary 'inputLength' var

--- src/algorithms/binarySearch.ts
+++ src/algorithms/binarySearch.ts
@@ -1,10 +1,8 @@
 const binarySearch = (
   input: number[], targetValue: number, lowIndex: number = 0, hightIndex: number = input.length
 ): number | null => {
-  const inputLength = input.length
-
-  if (inputLength === 0) return null
-  else if (inputLength === 1) return input[0] === targetValue ? 0 : null
+  if (input.length === 0) return null
+  else if (input.length === 1) return input[0] === targetValue ? 0 : null
 
   const middleItemIndex = Math.round((lowIndex + hightIndex) / 2)
   const middleItemValue = input[middleItemIndex]
