d13beeabd7ab46a4af36eb1496e5b62866506318
emil14 <emil.musician@gmail.com>
2018-08-20 20:51:57 +0300

    Fix TS Err in Hash Table

--- src/dataStructures/HashTable.ts
+++ src/dataStructures/HashTable.ts
@@ -1,17 +1,19 @@
-type HashTable = {
-  add: (key: string, value: any) => void,
+interface HashTable {
+  insert: (key: string, value: any) => void,
   read: (key: string) => any
 }
 
-const hashFunction = (str: string): number => str.split().reduce((acc, char) => acc + char.charCodeAt(0), 0)
+function hashFunction(str: string): number {
+  return str.split('').reduce((acc, char) => acc + char.charCodeAt(0), 0);
+}
 
-const HashTable = (): HashTable => {
-  const data: any[] = []
+function createHashTable(): HashTable {
+  const data: any[] = [];
 
   return {
-    add: (key, value) => data[hashFunction(key)] = value,
+    insert: (key, value) => data[hashFunction(key)] = value,
     read: key => data[hashFunction(key)]
   }
 }
 
-export default HashTable
+export default createHashTable;
