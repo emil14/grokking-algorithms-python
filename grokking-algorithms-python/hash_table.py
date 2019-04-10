51c4ec53109e3a9bd0a033574c873865b861cc76
emil14 <emil.musician@gmail.com>
2019-04-10 19:54:15 +0300

    Update hashtable

--- grokking-algorithms-python/hash_table.py
+++ grokking-algorithms-python/hash_table.py
@@ -1,26 +1,26 @@
+from functools import reduce
+
 class HashTable:
-    def __init__(self):
-        self._data = []
+    def __init__(self, size=23):
+        self.__data = []
+        self.__size = size
 
     def __repr__(self):
-        return f'<HashTable {self._data}>'
+        return f'<HashTable {self.__data}>'
 
     def __hash(key):
-        return len(key)
+        return reduce(lambda x, y: ord(x) + ord(y), 0) % self.__size
 
     def get(self, key):
         idx = self.__hash(key)
-        for (pair_key, pair_value) in self._data[idx]:
-            if pair_key == key:
-                return pair_value
-        return None
+        return next(pair_value for (pair_key, pair_value) in self.__data[idx] if pair_key == key, None)
 
     def set(self, key, value):
         idx = self.__hash(key)
         pair = (key, value)
-        if (len(self._data) >= idx):
-            self._data[idx].append(pair)
+        if (len(self.__data) >= idx):
+            self.__data[idx].append(pair)
         else:
-            self._data[idx] = [pair]
+            self.__data[idx] = [pair]
 
 
